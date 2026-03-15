a5a48866623c9133e3af16a2142df21cb4f12f54
ueno0112 <263294622+ueno0112@users.noreply.github.com>
2026-03-15 22:19:01 +0900

    TRAVELERの追記

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Traveler
   class Application < Rails::Application
+    config.active_storage.variant_processor = :vips
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
@@ -24,4 +25,5 @@ module Traveler
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  #get 'posts/new' 削除 
+  #post 'posts' => 'posts#create' 削除
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +10,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root :to => 'homes#top'
 end
